0c496c9be747f65339dff24e1e63f80ff944086a
wanggao <wgjiachen@163.com>
2026-08-19 11:59:37 +0800

    refactor: isolate new site resources

--- src/api/APITest.ts
+++
@@ -1,6 +0,0 @@
-//@k-url /APITest
-// sample code.. 
-var cateogryList = k.content.EcommerceComparisonCategory.all();
-var functionsList = cateogryList[2].EcommerceComparison;
-k.response.json(functionsList);
-k.response.json(cateogryList);
--- src/api/testip.ts
+++
@@ -1,9 +0,0 @@
-//@k-url /testip
-// sample code.. 
-var city = k.net.IP.getCityOrCountry(k.request.clientIp);
-if (city.countryCode == "CN") {
-  k.response.write("True");
-} else {
-  k.response.write("False");
-}
-k.response.write(city);
